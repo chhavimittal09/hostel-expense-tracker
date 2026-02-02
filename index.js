c57ee2da5de7bcc80037aeda1a943c287239bce7
Chhavi Mittal <chhavimittal0910@gmail.com>
2026-02-02 10:38:38 +0530

    updated login button and changed few features

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const studentIdInput = document.getElementById('studentId');
   const passwordInput = document.getElementById('password');
 
+document.addEventListener("mousemove", (e) => {
+   const x = (e.clientX / window.innerWidth - 0.5) * 10;
+   const y = (e.clientY / window.innerHeight - 0.5) * 10;
+   document.querySelector(".ambient-overlay").style.transform =
+      `translate(${x}px, ${y}px)`;
+  });
+
   // Check if already logged in
   const isLoggedIn = localStorage.getItem('isLoggedIn');
   if (isLoggedIn === 'true') {
